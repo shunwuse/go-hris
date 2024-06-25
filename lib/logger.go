6bef7a1658d8afe539b084e1a1d3418c8fc726f5
shun <shunwuse@gmail.com>
2024-06-25 12:39:04 +0800

    chore: Update log output path and create logs directory if it doesn't exist

--- lib/logger.go
+++ lib/logger.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 )
@@ -27,6 +28,14 @@ func newLogger(env Env) Logger {
 
 	logger.SetFormatter(&logrus.JSONFormatter{})
 	logger.SetLevel(logrus.DebugLevel)
+
+	// get directory path
+	dir := filepath.Dir(env.LogOutput)
+
+	// check directory logs exists, if not create it
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, os.ModePerm)
+	}
 
 	file, err := os.OpenFile(env.LogOutput, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
