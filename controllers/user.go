4e0329d3d7a36f94230adf425ecb5a6c26171898
shun <shunwuse@gmail.com>
2024-06-25 20:52:07 +0800

    feat: Add user DTO

--- controllers/user.go
+++ controllers/user.go
@@ -4,9 +4,11 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shunwuse/go-hris/dtos"
 	"github.com/shunwuse/go-hris/lib"
 	"github.com/shunwuse/go-hris/models"
 	"github.com/shunwuse/go-hris/services"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserController struct {
@@ -28,9 +30,9 @@ func NewUserController() UserController {
 
 // CreateUser controller
 func (c UserController) CreateUser(ctx *gin.Context) {
-	var user models.User
+	var userCreate dtos.UserCreate
 
-	if err := ctx.ShouldBindJSON(&user); err != nil {
+	if err := ctx.ShouldBindJSON(&userCreate); err != nil {
 		c.logger.Errorf("Error binding user: %v", err)
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"error": "Invalid request",
@@ -38,7 +40,21 @@ func (c UserController) CreateUser(ctx *gin.Context) {
 		return
 	}
 
-	if err := c.userService.CreateUser(&user); err != nil {
+	hashedPassword, err := hashPassword(userCreate.Password)
+	if err != nil {
+		c.logger.Errorf("Error hashing password: %v", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Error hashing password",
+		})
+	}
+
+	var user = &models.User{
+		Username:     userCreate.Username,
+		PasswordHash: hashedPassword,
+		Name:         userCreate.Name,
+	}
+
+	if err := c.userService.CreateUser(user); err != nil {
 		c.logger.Errorf("Error creating user: %v", err)
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Error creating user",
@@ -49,4 +65,13 @@ func (c UserController) CreateUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, gin.H{
 		"message": "create successfully",
 	})
+}
+
+func hashPassword(password string) (string, error) {
+	// Use bcrypt to hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
 }
---
+++ dtos/user.go
@@ -0,0 +1,7 @@
+package dtos
+
+type UserCreate struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Name     string `json:"name" binding:"required"`
+}
