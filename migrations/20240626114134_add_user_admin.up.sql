334598fd1aa610073a310e2f62347c89033a1296
shun <shunwuse@gmail.com>
2024-06-26 20:46:22 +0800

    chore: Update password to hashed

--- migrations/20240626114134_add_user_admin.up.sql
+++ migrations/20240626114134_add_user_admin.up.sql
@@ -1,3 +1,3 @@
 -- sqlite
 -- add data to the users table
-INSERT INTO users (username, password_hash, name) VALUES ('admin', 'password', 'Admin');
+INSERT INTO users (username, password_hash, name) VALUES ('admin', '$2a$10$vGD7tC3abULrbNuwNa//SO5AP72x5iQa84Kw9LYkl6YTNAJDVK1U6', 'Admin');
