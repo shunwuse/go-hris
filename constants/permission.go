6342c98243eb2f74194d49c13c24dda6958b46e5
shun <shunwuse@gmail.com>
2024-06-28 03:07:12 +0800

    feat: Add constants for permissions

---
+++ constants/permission.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	PermissionCreateUser     = "create_user"
+	PermissionReadUser       = "read_user"
+	PermissionUpdateUser     = "update_user"
+	PermissionCreateApproval = "create_approval"
+	PermissionReadApproval   = "read_approval"
+	PermissionActionApproval = "action_approval"
+)
