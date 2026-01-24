2f0aca31aeba62ac66cfff5c315885b2983df8a9
shun <shunwuse@gmail.com>
2026-01-25 02:43:08 +0800

    refactor: remove redundant context key

--- internal/constants/context.go
+++ internal/constants/context.go
@@ -11,7 +11,4 @@ const (
 
 	// JWTClaims is the context key for storing JWT claims.
 	JWTClaims contextKey = "claims"
-
-	// DBTransaction is the context key for storing database transaction.
-	DBTransaction contextKey = "db_transaction"
 )
